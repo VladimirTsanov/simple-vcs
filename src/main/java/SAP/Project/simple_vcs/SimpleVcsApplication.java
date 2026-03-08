f2b993947df1fe3c1384e377efb164cf33419368
Azgorn <damian.vladimirov@gmail.com>
2026-03-08 17:37:14 +0200

    init project

---
+++ src/main/java/SAP/Project/simple_vcs/SimpleVcsApplication.java
@@ -0,0 +1,13 @@
+package SAP.Project.simple_vcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleVcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SAP/Project/simple_vcs/SimpleVcsApplicationTests.java
@@ -0,0 +1,13 @@
+package SAP.Project.simple_vcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleVcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
