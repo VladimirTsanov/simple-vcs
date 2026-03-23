36e1ddcd65e66503708874d2598160eb2b4d2319
Ivandole <Vankoacctwo@gmail.com>
2026-03-23 21:00:59 +0200

    mhm

---
+++ src/main/java/SAP/Project/simple_vcs/security/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package SAP.Project.simple_vcs.security;
+
+import SAP.Project.simple_vcs.entity.Role;
+import SAP.Project.simple_vcs.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> {
+                    String roleName = "ROLE_" + role.getName();
+                    return new SimpleGrantedAuthority(roleName);
+                })
+                .collect(Collectors.toList());
+
+        return authorities;
+    }
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
+
+
